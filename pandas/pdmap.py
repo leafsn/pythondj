6b347cb820e7b7595f483cf9fc101e405e526ff1
xianjiang <719570853@qq.com>
2019-05-09 17:34:42 +0800

    提交

--- pandas/pdmap.py
+++ pandas/pdmap.py
@@ -461,5 +461,42 @@ print(df.apply(pd.to_timedelta))
 df = pd.DataFrame({
     'string': list('abc'),
     'int64': list(range(1, 4)),
-
+    'uint8': np.arange(3, 6).astype('u1'),
+    'float63': np.arange(4.0, 7.0),
+    'bool1': [True, False, True],
+    'bool2': [False, False, True],
+    'dates': pd.date_range('now', periods=3),
+    'category': pd.Series(list('ABC')).astype('category')
 })
+
+df['tdeltas'] = df.dates.diff()
+df['uint64'] = np.arange(3, 6).astype('u8')
+df['other_dates'] = pd.date_range('20130101', periods=3)
+df['tz_aware_dates'] = pd.date_range('20130101', periods=3, tz='US/Eastern')
+
+print(df)
+
+print(df.dtypes)
+
+## select_dtypes() 有两个参数  include, exclude
+print(df.select_dtypes(include=[bool]))
+
+print(df.select_dtypes(include=['bool']))
+
+print(
+    df.select_dtypes(include=['number', 'bool'], exclude=['unsignedinteger'])
+)
+
+print(
+    df.select_dtypes(include=['object'])
+)
+
+def subdtypes(dtype):
+    subs = dtype.__subclasses__()
+    if not subs:
+        return dtype
+    return [dtype, [subdtypes(dt) for dt in subs]]
+
+print(
+    subdtypes(np.generic)
+)
